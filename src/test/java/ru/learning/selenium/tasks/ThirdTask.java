c99ae2868460e125ae26001a0f3092e0a1c807d9
NataliaStudenikina <studenikin.n.a@gmail.com>
2020-09-27 17:53:46 +0300

    changed  third task

--- src/test/java/ru/learning/selenium/tasks/ThirdTask.java
+++ src/test/java/ru/learning/selenium/tasks/ThirdTask.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.By;
 public class ThirdTask extends TestBase {
 
     public static final String LOGIN_DETAILS ="admin";
+    public static final String LOCAL_HOST = "http://192.168.64.2/public_html/admin/";
 
     @Test
     public void fillingLogin() throws Exception {
-        driver.get("http://192.168.64.2/public_html/admin/");
+        driver.get(LOCAL_HOST);
         driver.findElement(By.name("username")).sendKeys(LOGIN_DETAILS);
         driver.findElement(By.name("password")).sendKeys(LOGIN_DETAILS);
         driver.findElement(By.name("login")).click();
