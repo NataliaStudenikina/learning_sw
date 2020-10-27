48756776e19335e972cd48ee5359068648fc9280
NataliaStudenikina <studenikin.n.a@gmail.com>
2020-10-27 22:27:44 +0300

    Added thirteenth task

--- src/test/java/ru/learning/selenium/tasks/TestBase.java
+++ src/test/java/ru/learning/selenium/tasks/TestBase.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
-
 public class TestBase {
 
     protected static WebDriver driver;
